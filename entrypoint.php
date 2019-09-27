fa89a1923655a25e9ef33686632e367315fd07cc
Dovydas Rupsys <dovydas.rupsys@mvfglobal.com>
2019-09-27 14:46:39 +0100

    Setup application class

--- entrypoint.php
+++ entrypoint.php
@@ -1,3 +1,12 @@
 <?php
 
+use Src\Application;
+
 require __DIR__ . '/vendor/autoload.php';
+
+$code = Application::run();
+if ($code > 0) {
+    echo "Application exited with non zero code " . $code . PHP_EOL;
+} else {
+    echo "Application completed" . PHP_EOL;
+}
---
+++ src/Application.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Src;
+
+class Application
+{
+    public static function run(): int {
+        return 0;
+    }
+}
